cdc2b0df4791ce71e60c95f7a9d93c8d11b3a8e0
Kripansh Baghel <kripanshbghl@gmail.com>
2026-03-30 08:17:38 +0000

    starting...

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@portfolio-vide-code/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
